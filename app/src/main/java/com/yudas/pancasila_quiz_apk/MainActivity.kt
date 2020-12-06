1c810dc7ec9ce59258ac558c2107f064ea6f3829
Yudas <yudasmalabi@gmail.com>
2020-12-06 22:59:52 +0700

    update mainActivity

--- app/src/main/java/com/yudas/pancasila_quiz_apk/MainActivity.kt
+++ app/src/main/java/com/yudas/pancasila_quiz_apk/MainActivity.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yudas.pancasila_quiz_apk.auth.RegisterActivity
+import com.yudas.pancasila_quiz_apk.menu.PengaturanActivity
+import com.yudas.pancasila_quiz_apk.menu.QuizActivity
+import com.yudas.pancasila_quiz_apk.menu.RankingActivity
+import com.yudas.pancasila_quiz_apk.menu.TutorialActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +16,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         quiz.setOnClickListener{
-            val redirectlogin = Intent(this@MainActivity, RegisterActivity::class.java)
-            startActivity(redirectlogin)
+            val intent = Intent(this@MainActivity, QuizActivity::class.java)
+            startActivity(intent)
+        }
+
+        ranking.setOnClickListener{
+            val intent = Intent(this@MainActivity, RankingActivity::class.java)
+            startActivity(intent)
+        }
+
+        pengaturan.setOnClickListener {
+            val intent = Intent(this@MainActivity, PengaturanActivity::class.java)
+            startActivity(intent)
+        }
+
+        tutorial.setOnClickListener{
+            val intent = Intent(this@MainActivity, TutorialActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/yudas/pancasila_quiz_apk/menu/PengaturanActivity.kt
@@ -0,0 +1,12 @@
+package com.yudas.pancasila_quiz_apk.menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yudas.pancasila_quiz_apk.R
+
+class PengaturanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pengaturan)
+    }
+}

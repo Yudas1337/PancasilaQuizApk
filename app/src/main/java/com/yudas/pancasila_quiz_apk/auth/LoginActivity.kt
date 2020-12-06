d22e82a69589525e3487b94cb86e800f9ffcb2a1
Yudas <yudasmalabi@gmail.com>
2020-12-06 15:08:15 +0700

    update auth package

---
+++ app/src/main/java/com/yudas/pancasila_quiz_apk/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.yudas.pancasila_quiz_apk.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yudas.pancasila_quiz_apk.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}

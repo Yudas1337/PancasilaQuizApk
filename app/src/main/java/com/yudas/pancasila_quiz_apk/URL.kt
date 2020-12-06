11dacd71335f7305618f9087e88942af46702dea
Yudas <yudasmalabi@gmail.com>
2020-12-06 20:01:16 +0700

    object url

---
+++ app/src/main/java/com/yudas/pancasila_quiz_apk/URL.kt
@@ -0,0 +1,11 @@
+package com.yudas.pancasila_quiz_apk
+object URL {
+
+    //var IP = "https://techpedia.pecelsec.com/assets/"
+    var IP = "http://192.168.1.4//quizPancasila/"
+    var rest = IP + "rest/"
+    var assets = IP + "assets/img/"
+    var gambarSoal = assets + "soal/"
+    var fotoProfil = assets + "foto_user/"
+
+}

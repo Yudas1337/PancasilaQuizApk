48a7f038fd4802d03099cc6f596e5f6d85c1092c
Yudas <yudasmalabi@gmail.com>
2020-12-06 13:21:40 +0700

    add splashActivity

---
+++ app/src/main/java/com/yudas/pancasila_quiz_apk/intro/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.yudas.pancasila_quiz_apk.intro
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import com.yudas.pancasila_quiz_apk.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.activity_splash)
+        val handler = Handler()
+        handler.postDelayed({
+            startActivity(Intent(applicationContext, WelcomeActivity::class.java))
+            finish()
+        }, 3000L) //3000 L = 3 detik
+    }
+}
